2d811a2b8b63670047505cb56c654a60c5adc624
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-09 19:59:56 +0530

    Added employee Wage Calculation using Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,14 +3,12 @@ isFullTime=2
 isPartTime=1
 ratePerHour=20
 empCheck=$((RANDOM%3))
-
-if [ $isFullTime -eq $empCheck ]
-then
-        empHrs=8;
-elif [ $isPartTime -eq $empCheck ]
-then
-        empHrs=4;
-else
-        empHrs=0;
-fi
+        case $empCheck in
+                $isFullTime)
+                        empHrs=8;;
+                $isPartTime)
+                        empHrs=4;;
+                *)
+                        empHrs=0;;
+        esac
 salary=$(($empHrs*$ratePerHour))
