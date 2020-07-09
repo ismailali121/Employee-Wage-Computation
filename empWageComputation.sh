8e11464b261d2d0efbe7294a79bc6c072a04b2df
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-09 20:38:57 +0530

    Refractored code with function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -x
 isFullTime=2
 isPartTime=1
-maxHrsInMonth=10
+maxHrsInMonth=4
 ratePerHour=20
 noOfWorkingDays=20
 
 totalEmpHrs=0
 totalWorkingDays=0;
 
-while [[ $totalEmpHrs -lt $maxHrsInMonth
-                                 && $totalWorkingDays -lt $noOfWorkingDays ]]
-do
-        ((totalWorkingDays++))
-        empCheck=$((RANDOM%3))
+function getWorkHrs(){
+        local $empCheck=$1
         case $empCheck in
                 $isFullTime)
                         empHrs=8;;
@@ -21,7 +18,17 @@ do
                 *)
                         empHrs=0;;
         esac
+        echo $empHrs
+        #totalEmpHrs=$(($totalEmpHrs+$empHrs))
+}
+while [[ $totalEmpHrs -lt $maxHrsInMonth
+                                 && $totalWorkingDays -lt $noOfWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3))
+        empHrs="$( getWorkHrs $empCheck )"
         totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
 
 totalSalary=$(($totalEmpHrs*$ratePerHour))
+echo $totalSalary
